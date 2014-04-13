7d559cb464465e3672395edb0a9ba90faa957feb
Alex Wells <thealee@gmail.com>
2014-04-13 20:45:38 +0100

    Trying to make realtime using my own MIDI sender. Sounding very different to generated files...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import midi.MidiSender;
 import context.Context;
 import context.ContextManager;
+import context.ContextThread;
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.*;
@@ -10,8 +12,16 @@ public final class Main {
 	public static void main(String[] args){
 		
 		ContextManager cManager = new ContextManager();
-		
-		
+		//MidiSender midi = new MidiSender(0,1);
+
+		ContextThread player1 = new ContextThread(cManager.c1);
+
+			//if (player1.getState() == Thread.State.NEW) {
+
+			player1.start();
+//			}
+
+
 		
 	}
 }
