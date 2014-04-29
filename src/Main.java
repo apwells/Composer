c8fdf13745a59c4e492e24e680f3cb682e5774c6
Alex Wells <thealee@gmail.com>
2014-04-29 20:57:54 +0100

    Part way through re-factoring program based on my UML diagram. Some code still stubbs

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
+import voice.VoiceManager;
 import midi.MidiSender;
 import context.Context;
 import context.ContextManager;
-import context.ContextThread;
 import jm.JMC;
 import jm.music.data.*;
 import jm.util.*;
 
 public final class Main {
 	
-	
 	public static void main(String[] args){
 		
-		ContextManager cManager = new ContextManager();
-		//MidiSender midi = new MidiSender(0,1);
-
-		ContextThread player1 = new ContextThread(cManager.c1);
-		ContextThread player2 = new ContextThread(cManager.c2);
-
-			//if (player1.getState() == Thread.State.NEW) {
-
-			player1.start();
-			player2.start();
+		Settings settings;
+		ContextManager contextManager;
+		VoiceManager voiceManager;
+		
+		settings = new Settings("Soundtrack1");
+		contextManager = settings.getContextManager();
+		voiceManager = settings.getVoiceManager();
+		
+//		ContextManager cManager = new ContextManager();
+//
+//		ContextThread player1 = new ContextThread(cManager.c1);
+//		ContextThread player2 = new ContextThread(cManager.c2);
+//
+//		player1.start();
+//		player2.start();
 		
 		// Little test
 //		Phrase phrase = new Phrase();
--- src/context/Context.java
+++ src/context/Context.java
@@ -1,4 +1,5 @@
 package context;
+import java.util.ArrayList;
 import java.util.Random;
 
 import analyser.Markov;
@@ -8,54 +9,15 @@ import jm.music.data.*;
 import jm.music.tools.*;
 import jm.util.*;
 
-public final class Context implements JMC{
-	
-	String source;
-	public Score theScore;
-	private Markov markov;
-	
-	Note[] noteArray;
-	float variation;
-	int voices;
-	float repetition;
-	float discord;
-	float excitement;
-	float thickness;
-	
-	// Take a file as input. Get it's notes/parts
-	// Later on we should make the source file denote our context variables through some clever analysis of its contents (idea)
-	public Context(String source) {
-		this.source = source;
-		this.theScore = new Score("ContextScore");
-		this.markov = new Markov(1, source);
-		
-		// Read.midi(theScore, source);
-		// Part tempPart = theScore.getPart(0);
-		
-		// View.show(theScore);
-		
+public final class Context{
+
+	private ArrayList<Line> lineList;
+	private int id;
+	private String name;
+
+	public Context(String name, ArrayList<Line> lineList) {
+		this.name = name;
+		this.lineList = lineList;
 	}
-	
-	// Initialize our variables
-	public void Init(float variation, int voices, float repetition, float discord, float excitement, float thickness, Note[] noteArray) {
-		this.variation = variation;
-		this.voices = voices;
-		this.repetition = repetition;
-		this.discord = discord;
-		this.excitement = excitement;
-		this.thickness = thickness;
-		this.noteArray = noteArray;
-	}
-	
-	// Complex algorithm...
-	public Note getNextNote() {
-//		Random rnd = new Random();
-//		int note = rnd.nextInt(512) % noteArray.length;	// Should mod to size;
-//		System.out.println("Note : "+note);
-//		return noteArray[note];
-		return markov.getNext();
-	}
-	
-	
 
 }
